df90d672bf56eabd5320542fd2ef290d92724cff
Jake Overall <joverall22@gmail.com>
2017-10-02 16:27:01 -0600

    changes id to string

--- Models/Keep.cs
+++ Models/Keep.cs
@@ -4,7 +4,7 @@ namespace keepr.Models
 {
     public class Keep
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string ImgUrl { get; set; }
