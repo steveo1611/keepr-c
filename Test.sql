258def3d07b39ddfb22489d4fb63da192adac05e
Mark R Ohnsman <MarkOhnsman@users.noreply.github.com>
2018-01-11 16:49:41 -0700

    Update Test.sql

--- Test.sql
+++ Test.sql
@@ -1,3 +1,59 @@
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT,
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY email (email)
+);
+
+CREATE TABLE vaults (
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    userId int,
+    INDEX userId (userId),
+    FOREIGN KEY (userId)
+        REFERENCES users(id)
+        ON DELETE CASCADE,  
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE keeps (
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    userId int,
+    INDEX userId (userId),
+    FOREIGN KEY (userId)
+        REFERENCES users(id)
+        ON DELETE CASCADE,  
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE vaultkeeps (
+    id int NOT NULL AUTO_INCREMENT,
+    vaultId int NOT NULL,
+    keepId int NOT NULL,
+    userId int NOT NULL,
+
+    PRIMARY KEY (id),
+    INDEX (vaultId, keepId),
+    INDEX (userId),
+
+    FOREIGN KEY (userId)
+        REFERENCES users(id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (vaultId)
+        REFERENCES vaults(id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (keepId)
+        REFERENCES keeps(id)
+        ON DELETE CASCADE
+)
+
 
 -- USE THIS LINE FOR GET KEEPS BY VAULTID
 SELECT * FROM vaultkeeps vk
